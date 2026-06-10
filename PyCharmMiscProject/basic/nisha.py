d3ad2c204ebd98a3ffc05f2beaba0d1bfaebcf6a
nisha8920 <nisha89202006@gmail.com>
2026-06-10 10:59:32 +0530

    initial commit

---
+++ PyCharmMiscProject/basic/nisha.py
@@ -0,0 +1,3 @@
+print("---------hello world---------")
+print("----------welcome nisha------------")
+2
--- PyCharmMiscProject/basic/waste.py
+++
@@ -1,3 +0,0 @@
-print("-----------hello-----------")
-print("Welcome to the Waste")
-print("---------------------------")

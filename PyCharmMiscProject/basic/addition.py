25cff654f1e14091ff5304d923e2fafed48a5172
nisha8920 <nisha89202006@gmail.com>
2026-06-10 11:21:24 +0530

    first commit

---
+++ PyCharmMiscProject/basic/addition.py
@@ -0,0 +1,33 @@
+print("-------------add the two numbers-------------")
+a=27
+b=45
+c=a+b
+print(c)
+print("-------------subtract the two numbers-------------")
+a=24
+b=26
+c=a-b
+print(c)
+print("-------------multiply the two numbers-------------")
+a=21
+b=36
+c=a*b
+print(c)
+print("-------------divide the two numbers-------------")
+a=34
+b=2
+c=a/b
+print(c)
+print("-------------area of the circle---------")
+r=25
+area=22/7*r**2
+print(area)
+print("-------------circumference of the circle---------")
+r=12
+circumference=2*22/7*r
+print(circumference)
+print("-----------area of cube-------------")
+s=38
+cube=s*s*s
+print(cube)
+

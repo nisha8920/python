2e8d428f08fa85bdc329e15fdfc75dbbb5b41660
nisha8920 <nisha89202006@gmail.com>
2026-06-11 10:41:38 +0530

    minor changes

--- PyCharmMiscProject/basic/kite.py
+++ PyCharmMiscProject/basic/kite.py
@@ -5,3 +5,15 @@ a=b
 b=c
 print(a)
 print(b)
+########password#############
+print("------------PASSWORD-----------")
+import random
+import string
+
+length = int(input("Enter Password Length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ''.join(random.choice(characters) for _ in range(length))
+
+print("Generated Password:", password)
